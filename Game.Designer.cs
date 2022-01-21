23bddd38289e0159a8fc9001ee4c23a7b3f41dcc
Clementine <klemstark@gmail.com>
2022-01-21 16:01:43 +0100

    1st commit

---
+++ Game.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Arkanoid
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.Color.White;
+            this.scoreLabel.Location = new System.Drawing.Point(13, 13);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(94, 24);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "Score : 0";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.LightGray;
+            this.player.Location = new System.Drawing.Point(347, 492);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 32);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Yellow;
+            this.ball.Location = new System.Drawing.Point(377, 297);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(25, 23);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timerEvent);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 536);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.scoreLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(816, 575);
+            this.MinimumSize = new System.Drawing.Size(816, 575);
+            this.Name = "Game";
+            this.Text = "Arkanoid";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer;
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Arkanoid
+{
+    public partial class Game : Form
+    {
+        bool goRight;
+        bool goLeft;
+        bool isInit;
+        bool isGameOver;
+
+        int score;
+        int ballX;
+        int ballY;
+        int playerSpeed;
+
+        Random random = new Random();
+        PictureBox[] blocks;
+
+        public Game()
+        {
+            isInit = true;
+            InitializeComponent();            
+        }
+
+        private void initGame()
+        {            
+            isGameOver = false;
+            score = 0;
+            ballX = 5;
+            ballY = 5;
+            playerSpeed = 12;
+            scoreLabel.Text = "Score: " + score;
+
+            ball.Left = 376;
+            ball.Top = 328;
+
+            player.Left = 347;
+
+            timer.Start();
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "blocks")
+                {
+                    x.BackColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+                }
+            }
+        }
+
+        private void gameOver(string message)
+        {
+            isGameOver = true;
+            timer.Stop();
+            scoreLabel.Text = "Score: " + score + " " + message;
+        }
+
+        private void initBlocks()
+        {
+            blocks = new PictureBox[15];
+
+            int col = 0;
+            int top = 50;
+            int left = 100;
+
+            for (int i = 0; i < blocks.Length; i++)
+            {
+                blocks[i] = new PictureBox();
+                blocks[i].Height = 32;
+                blocks[i].Width = 100;
+                blocks[i].Tag = "blocks";
+                blocks[i].BackColor = Color.White;
+
+                if (col == 5)
+                {
+                    top += 50;
+                    left = 100;
+                    col = 0;
+                }
+
+                if (col < 5)
+                {
+                    col++;
+                    blocks[i].Left = left;
+                    blocks[i].Top = top;
+                    this.Controls.Add(blocks[i]);
+                    left = left + 130;
+                }
+            }
+            initGame();
+        }
+
+        private void removeBlocks()
+        {
+            foreach (PictureBox x in blocks)
+            {
+                this.Controls.Remove(x);
+            }
+        }
+
+        private void timerEvent(object sender, EventArgs e)
+        {
+            scoreLabel.Text = "Score : " + score;
+
+            if (goLeft == true && player.Left > 0)
+            {
+                player.Left -= playerSpeed;
+            }
+
+            if (goRight == true && player.Left < 700)
+            {
+                player.Left += playerSpeed;
+            }
+
+            ball.Left += ballX;
+            ball.Top += ballY;
+
+            if (ball.Left < 0 || ball.Left > 775)
+            {
+                ballX = -ballX;
+            }
+            if (ball.Top < 0)
+            {
+                ballY = -ballY;
+            }
+
+            if (ball.Bounds.IntersectsWith(player.Bounds))
+            {
+                ball.Top = 463;
+
+                ballY = random.Next(5, 12) * -1;
+
+                if (ballX < 0)
+                {
+                    ballX = random.Next(5, 12) * -1;
+                }
+                else
+                {
+                    ballX = random.Next(5, 12);
+                }
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "blocks")
+                {
+                    if (ball.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        score += 1;
+
+                        ballY = -ballY;
+
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+
+            if (score == 15)
+            {
+                gameOver("You Win ! Press Enter to play again");
+            }
+
+            if (ball.Bottom > 580)
+            {
+                gameOver("You Lose ! Press Enter to try again");
+            }
+        }
+
+        private void keyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void keyUp(object sender, KeyEventArgs e)
+        {
+            // start game when player is ready
+            if(isInit)
+            {
+                initBlocks();
+                isInit = false;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                removeBlocks();
+                initBlocks();
+            }
+        }
+    }
+}
